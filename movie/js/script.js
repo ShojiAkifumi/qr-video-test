608c5bff440838f9abc78fe13d2b6a19c71b6cff
CAI63\akifumis <akifumis@smocca.co.jp>
2024-06-25 13:58:28 +0900

    vide muted

---
+++ movie/js/script.js
@@ -0,0 +1,58 @@
+const video = document.getElementById('video');
+const message = document.getElementById('message');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const recordedVideo = document.getElementById('recordedVideo');
+
+let mediaRecorder;
+let recordedChunks = [];
+
+// カメラストリームを取得してビデオ要素に表示する
+navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: 'environment' // 外側カメラを指定
+    },
+    audio: true
+})
+.then(stream => {
+    message.hidden = true;
+    video.srcObject = stream;
+
+    // MediaRecorderを初期化
+    mediaRecorder = new MediaRecorder(stream);
+
+    // データが利用可能になったときにデータを収集する
+    mediaRecorder.ondataavailable = event => {
+        if (event.data.size > 0) {
+            recordedChunks.push(event.data);
+        }
+    };
+
+    // 録画が停止したときにビデオを再生可能にする
+    mediaRecorder.onstop = () => {
+        const blob = new Blob(recordedChunks, { type: 'video/webm' });
+        recordedChunks = [];
+        const url = URL.createObjectURL(blob);
+        recordedVideo.src = url;
+    };
+})
+.catch(error => {
+    window.alert('Error accessing media devices.', error);
+    console.error('Error accessing media devices.', error);
+});
+
+// 録画開始
+startBtn.addEventListener('click', () => {
+    mediaRecorder.start();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    recordedVideo.hidden = true;
+});
+
+// 録画停止
+stopBtn.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    recordedVideo.hidden = false;
+});
---
+++ qr/js/script.js
@@ -0,0 +1,43 @@
+var canvasElement = document.getElementById("canvas");
+var video = document.createElement("video");
+var canvas = canvasElement.getContext("2d");
+var message = document.getElementById("message");
+var reload = document.getElementById("reload");
+
+if(navigator.mediaDevices){
+   // カメラストリームを取得してビデオ要素に表示する
+   navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+      video.srcObject = stream;
+      video.setAttribute("playsinline", true);
+      video.play();
+      requestAnimationFrame(tick);
+   });
+}else{
+   message.innerText = "🎥 デバイスカメラにアクセスできません (カメラへのアクセスを許可してください)"
+}
+
+
+function tick() {
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    message.hidden = true;
+    canvasElement.hidden = false;
+
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+    // カメラストリームから取得した画像を解析
+    var code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: "dontInvert",
+    });
+    // QRコードの文字列の先頭が「http」であれば文字列を表示する
+    if (code && code.data.startsWith('http')) {
+      canvasElement.hidden = true;
+      message.hidden = false;
+      reload.hidden = false;
+      message.innerText = code.data;
+      return 0;
+    }
+  }
+  requestAnimationFrame(tick);
+}
